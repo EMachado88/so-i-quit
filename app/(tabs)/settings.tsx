09c28386912f37c84ad50abbda1d47d0668665bf
Emanuel Machado <emanuel@emachado.pt>
2026-03-27 20:50:03 +0000

    Fixed reset savings

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,15 +1,15 @@
-import {Alert, StyleSheet} from 'react-native';
+import { Alert, StyleSheet } from "react-native";
 
-import {ThemedText} from '@/components/themed-text';
-import {ThemedView} from '@/components/themed-view';
-import {DarkTheme} from "@react-navigation/native";
-import {DateTimePickerAndroid} from "@react-native-community/datetimepicker";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { fetchSettings } from "@/utils/fetch-settings";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {useEffect, useState} from "react";
-import {fetchSettings} from "@/utils/fetch-settings";
+import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
+import { DarkTheme } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import Button from "react-native-paper/src/components/Button/Button";
 import Divider from "react-native-paper/src/components/Divider";
 import TextInput from "react-native-paper/src/components/TextInput/TextInput";
-import Button from "react-native-paper/src/components/Button/Button";
 
 export default function TabTwoScreen() {
   const [soberDate, setSoberDate] = useState<string | null>(null);
@@ -17,182 +17,267 @@ export default function TabTwoScreen() {
   const [smokeDate, setSmokeDate] = useState<string | null>(null);
   const [smokeSavings, setSmokeSavings] = useState<string | null>(null);
 
-  useEffect(() => fetchSettings(setSoberDate, setSoberSavings, setSmokeDate, setSmokeSavings), []);
+  useEffect(
+    () =>
+      fetchSettings(
+        setSoberDate,
+        setSoberSavings,
+        setSmokeDate,
+        setSmokeSavings,
+      ),
+    [],
+  );
 
   return (
-    <ThemedView
-      style={styles.pageWrapper}>
+    <ThemedView style={styles.pageWrapper}>
       <ThemedView>
-        <ThemedText
-          type="title">
-          Settings
-        </ThemedText>
+        <ThemedText type="title">Settings</ThemedText>
       </ThemedView>
 
-      <Divider/>
+      <Divider />
 
       <ThemedView>
-        <ThemedText
-          type="subtitle" style={{marginBottom: 15}}>
+        <ThemedText type="subtitle" style={{ marginBottom: 15 }}>
           Alcohol free
         </ThemedText>
         <ThemedView
-          style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginBottom: 10}}>
-          <ThemedView style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Button compact={true} icon="pencil" mode="text"
-                    contentStyle={{flexDirection: 'row-reverse'}}
-                    onPress={() => DateTimePickerAndroid.open({
-                      mode: 'date',
-                      value: soberDate ? new Date(soberDate) : new Date(),
-                      maximumDate: new Date(),
-                      style: {backgroundColor: DarkTheme.colors.card},
-                      onChange: async (_event, date) => {
-                        if (date) {
-                          setSoberDate(date.toISOString());
-                          await AsyncStorage.setItem('soberDate', date.toISOString());
-                        }
-                      }
-                    })}
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: 10,
+          }}
+        >
+          <ThemedView style={{ flexDirection: "row", alignItems: "center" }}>
+            <Button
+              compact={true}
+              icon="pencil"
+              mode="text"
+              contentStyle={{ flexDirection: "row-reverse" }}
+              onPress={() =>
+                DateTimePickerAndroid.open({
+                  mode: "date",
+                  value: soberDate ? new Date(soberDate) : new Date(),
+                  maximumDate: new Date(),
+                  style: { backgroundColor: DarkTheme.colors.card },
+                  onChange: async (_event, date) => {
+                    if (date) {
+                      setSoberDate(date.toISOString());
+                      await AsyncStorage.setItem(
+                        "soberDate",
+                        date.toISOString(),
+                      );
+                    }
+                  },
+                })
+              }
             >
-              {soberDate ? new Date(soberDate).toLocaleDateString() : "--/--/----"}
+              {soberDate
+                ? new Date(soberDate).toLocaleDateString()
+                : "--/--/----"}
             </Button>
           </ThemedView>
-          <ThemedView style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Button compact={true} icon="pencil" mode="text"
-                    contentStyle={{flexDirection: 'row-reverse'}}
-                    onPress={() => DateTimePickerAndroid.open({
-                      mode: 'time',
-                      value: soberDate ? new Date(soberDate) : new Date(),
-                      maximumDate: new Date(),
-                      style: {backgroundColor: DarkTheme.colors.card},
-                      onChange: async (_event, date) => {
-                        if (date) {
-                          setSoberDate(date.toISOString());
-                          await AsyncStorage.setItem('soberDate', date.toISOString());
-                        }
-                      }
-                    })}
+          <ThemedView style={{ flexDirection: "row", alignItems: "center" }}>
+            <Button
+              compact={true}
+              icon="pencil"
+              mode="text"
+              contentStyle={{ flexDirection: "row-reverse" }}
+              onPress={() =>
+                DateTimePickerAndroid.open({
+                  mode: "time",
+                  value: soberDate ? new Date(soberDate) : new Date(),
+                  maximumDate: new Date(),
+                  style: { backgroundColor: DarkTheme.colors.card },
+                  onChange: async (_event, date) => {
+                    if (date) {
+                      setSoberDate(date.toISOString());
+                      await AsyncStorage.setItem(
+                        "soberDate",
+                        date.toISOString(),
+                      );
+                    }
+                  },
+                })
+              }
             >
-              {soberDate ? new Date(soberDate).toLocaleTimeString() : '--:--'}
+              {soberDate ? new Date(soberDate).toLocaleTimeString() : "--:--"}
             </Button>
           </ThemedView>
         </ThemedView>
-        <ThemedView style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
+        <ThemedView
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <TextInput
             label="Savings"
-            value={soberSavings || undefined}
+            value={soberSavings || ""}
             keyboardType="number-pad"
             placeholder="- - "
             mode="outlined"
-            right={<TextInput.Affix text="€/day"/>}
+            right={<TextInput.Affix text="€/day" />}
             onChangeText={async (text) => {
-              const absValue = text.replace(/[^0-9.]/g, '');
-              setSoberSavings(absValue)
-              await AsyncStorage.setItem('soberSavings', absValue)
+              const absValue = text.replace(/[^0-9.]/g, "");
+              setSoberSavings(absValue);
+              await AsyncStorage.setItem("soberSavings", absValue);
             }}
           />
 
-          <Button compact={true} icon="refresh" contentStyle={{flexDirection: 'row-reverse'}} onPress={() => {
-            Alert.alert("Reset Sober Date", "Are you sure you want to reset your sober date?", [
-              {
-                text: "Cancel",
-                style: "cancel"
-              },
-              {
-                text: "Reset",
-                style: "destructive",
-                onPress: async () => {
-                  setSoberDate(null);
-                  await AsyncStorage.removeItem('soberDate');
-                  setSoberSavings(null);
-                  await AsyncStorage.removeItem('soberSavings');
-                }
-              }
-            ])
-          }}>Reset</Button>
+          <Button
+            compact={true}
+            icon="refresh"
+            contentStyle={{ flexDirection: "row-reverse" }}
+            onPress={() => {
+              Alert.alert(
+                "Reset Sober Date",
+                "Are you sure you want to reset your sober date?",
+                [
+                  {
+                    text: "Cancel",
+                    style: "cancel",
+                  },
+                  {
+                    text: "Reset",
+                    style: "destructive",
+                    onPress: async () => {
+                      setSoberDate(null);
+                      await AsyncStorage.removeItem("soberDate");
+                      setSoberSavings(null);
+                      await AsyncStorage.removeItem("soberSavings");
+                    },
+                  },
+                ],
+              );
+            }}
+          >
+            Reset
+          </Button>
         </ThemedView>
       </ThemedView>
 
-      <Divider/>
+      <Divider />
 
       <ThemedView>
-        <ThemedText
-          type="subtitle" style={{marginBottom: 15}}>
+        <ThemedText type="subtitle" style={{ marginBottom: 15 }}>
           Smoke free
         </ThemedText>
         <ThemedView
-          style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginBottom: 10}}>
-          <ThemedView style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Button compact={true} icon="pencil" mode="text"
-                    contentStyle={{flexDirection: 'row-reverse'}}
-                    onPress={() => DateTimePickerAndroid.open({
-                      mode: 'date',
-                      value: smokeDate ? new Date(smokeDate) : new Date(),
-                      maximumDate: new Date(),
-                      style: {backgroundColor: DarkTheme.colors.card},
-                      onChange: async (_event, date) => {
-                        if (date) {
-                          setSmokeDate(date.toISOString());
-                          await AsyncStorage.setItem('smokeDate', date.toISOString());
-                        }
-                      }
-                    })}
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: 10,
+          }}
+        >
+          <ThemedView style={{ flexDirection: "row", alignItems: "center" }}>
+            <Button
+              compact={true}
+              icon="pencil"
+              mode="text"
+              contentStyle={{ flexDirection: "row-reverse" }}
+              onPress={() =>
+                DateTimePickerAndroid.open({
+                  mode: "date",
+                  value: smokeDate ? new Date(smokeDate) : new Date(),
+                  maximumDate: new Date(),
+                  style: { backgroundColor: DarkTheme.colors.card },
+                  onChange: async (_event, date) => {
+                    if (date) {
+                      setSmokeDate(date.toISOString());
+                      await AsyncStorage.setItem(
+                        "smokeDate",
+                        date.toISOString(),
+                      );
+                    }
+                  },
+                })
+              }
             >
-              {smokeDate ? new Date(smokeDate).toLocaleDateString() : "--/--/----"}
+              {smokeDate
+                ? new Date(smokeDate).toLocaleDateString()
+                : "--/--/----"}
             </Button>
           </ThemedView>
-          <ThemedView style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Button compact={true} icon="pencil" mode="text"
-                    contentStyle={{flexDirection: 'row-reverse'}}
-                    onPress={() => DateTimePickerAndroid.open({
-                      mode: 'time',
-                      value: smokeDate ? new Date(smokeDate) : new Date(),
-                      maximumDate: new Date(),
-                      style: {backgroundColor: DarkTheme.colors.card},
-                      onChange: async (_event, date) => {
-                        if (date) {
-                          setSmokeDate(date.toISOString());
-                          await AsyncStorage.setItem('smokeDate', date.toISOString());
-                        }
-                      }
-                    })}
+          <ThemedView style={{ flexDirection: "row", alignItems: "center" }}>
+            <Button
+              compact={true}
+              icon="pencil"
+              mode="text"
+              contentStyle={{ flexDirection: "row-reverse" }}
+              onPress={() =>
+                DateTimePickerAndroid.open({
+                  mode: "time",
+                  value: smokeDate ? new Date(smokeDate) : new Date(),
+                  maximumDate: new Date(),
+                  style: { backgroundColor: DarkTheme.colors.card },
+                  onChange: async (_event, date) => {
+                    if (date) {
+                      setSmokeDate(date.toISOString());
+                      await AsyncStorage.setItem(
+                        "smokeDate",
+                        date.toISOString(),
+                      );
+                    }
+                  },
+                })
+              }
             >
-              {smokeDate ? new Date(smokeDate).toLocaleTimeString() : '--:--'}
+              {smokeDate ? new Date(smokeDate).toLocaleTimeString() : "--:--"}
             </Button>
           </ThemedView>
         </ThemedView>
-        <ThemedView style={{flexDirection: "row", justifyContent: 'space-between', alignItems: "center"}}>
+        <ThemedView
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <TextInput
             label="Savings"
-            value={smokeSavings || undefined}
+            value={smokeSavings || "0"}
             keyboardType="number-pad"
             placeholder="- - "
             mode="outlined"
-            right={<TextInput.Affix text="€/day"/>}
+            right={<TextInput.Affix text="€/day" />}
             onChangeText={async (text) => {
-              const absValue = text.replace(/[^0-9.]/g, '');
-              setSmokeSavings(absValue)
-              await AsyncStorage.setItem('smokeSavings', absValue)
+              const absValue = text.replace(/[^0-9.]/g, "");
+              setSmokeSavings(absValue);
+              await AsyncStorage.setItem("smokeSavings", absValue);
             }}
           />
-          <Button compact={true} icon="refresh" contentStyle={{flexDirection: 'row-reverse'}} onPress={() => {
-            Alert.alert("Reset Smoke Date", "Are you sure you want to reset your smoke free date?", [
-              {
-                text: "Cancel",
-                style: "cancel"
-              },
-              {
-                text: "Reset",
-                style: "destructive",
-                onPress: async () => {
-                  setSmokeDate(null);
-                  await AsyncStorage.removeItem('smokeDate');
-                  setSmokeSavings(null);
-                  await AsyncStorage.removeItem('smokeSavings');
-                }
-              }
-            ])
-          }}>Reset</Button>
+          <Button
+            compact={true}
+            icon="refresh"
+            contentStyle={{ flexDirection: "row-reverse" }}
+            onPress={() => {
+              Alert.alert(
+                "Reset Smoke Date",
+                "Are you sure you want to reset your smoke free date?",
+                [
+                  {
+                    text: "Cancel",
+                    style: "cancel",
+                  },
+                  {
+                    text: "Reset",
+                    style: "destructive",
+                    onPress: async () => {
+                      setSmokeDate(null);
+                      await AsyncStorage.removeItem("smokeDate");
+                      setSmokeSavings(null);
+                      await AsyncStorage.removeItem("smokeSavings");
+                    },
+                  },
+                ],
+              );
+            }}
+          >
+            Reset
+          </Button>
         </ThemedView>
       </ThemedView>
     </ThemedView>
@@ -208,6 +293,6 @@ const styles = StyleSheet.create({
   input: {
     borderBottomWidth: 1,
     borderBottomColor: DarkTheme.colors.primary,
-    color: DarkTheme.colors.text
-  }
+    color: DarkTheme.colors.text,
+  },
 });
